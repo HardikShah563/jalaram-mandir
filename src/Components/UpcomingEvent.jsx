5923325c7e5fb5f4d01a81354853ecfae1ddc17d
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2023-11-11 22:40:35 +0530

    upcoming event

--- src/Components/UpcomingEvent.jsx
+++ src/Components/UpcomingEvent.jsx
@@ -1,7 +1,35 @@
+// importing icons
+import { ImLocation } from "react-icons/im";
+// importing stylesheet
+import "../style/events.css";
+
 export default function UpcomingEvent(props) {
     return (
         <>
-            <h1>Upcoming Event</h1>
+            <div className="outer">
+                <div className="event-big">
+                    <div className="upcoming-details">
+                        <div className="upcoming-event-name">{props.eventName}</div>
+                        <div className="flex gap-5">
+                            <ImLocation className="location-icon" />
+                            <div className="upcoming-event-location">{props.eventLocation}</div>
+                        </div>
+                        <div className="flex">
+                            <div className="upcoming-event-date-time">{props.eventDate}, {props.eventStartTime} onwards</div>
+                        </div>
+                    </div>
+                    <div className="upcoming-img-div">
+                        <img
+                            src={
+                                process.env.PUBLIC_URL +
+                                "/Assets/temple.png"
+                            }
+                            alt="upcoming event photo"
+                            className="upcoming-img-img"
+                        />
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,6 @@
+// importing components
+import Marquee from "../components/Marquee";
+// importing icons
 import { MdArrowForwardIos } from "react-icons/md";
 // importing stylesheet
 import "../style/home.css";
@@ -5,11 +8,13 @@ import { useNavigate } from "react-router-dom";
 import UpcomingEvent from "../components/UpcomingEvent";
 
 export default function Home() {
+    // creating variable responsible for web navigation
     const navigate = useNavigate();
 
     return (
         <>
             <div className="page">
+                {/* main section */}
                 <h1 className="txt-ctr title main-title">|| Jai Jalaram ||</h1>
                 <img
                     className="main-img w-100 mg-a-a"
@@ -20,23 +25,38 @@ export default function Home() {
                     alt="main-img"
                 />
 
+                {/* quote 1 section */}
                 <h1 className="main-quote txt-ctr">|| રામ નામ મેં લીન હૈ દેખત સબમે રામ ||<br />|| તાકે પદ વંદન કરે જય જય જલારામ ||</h1>
 
-                <div className="w-80">
+                {/* Events Section */}
+                <div className="w-80 mg-a-a">
                     <h1 className="subtitle main-title">Our <br />Upcoming Events</h1>
 
                     <div className="events">
-                        <UpcomingEvent />
+                        <UpcomingEvent 
+                            eventName={"Jalaram Jayanti 2023"}
+                            eventLocation={"Nilkanth Kutir Building, Bhandup Station Rd, Mumbai - 400078"}
+                            eventDate={"19th October 2023"}
+                            eventStartTime={"8:30"}
+                        />
                     </div>
 
                     <h1 className="small-heading">Past Events</h1>
 
                     <div className="flex jstf-ctr">
-                        <div className="black-button" onClick={navigate("/events")}>
+                        <div className="black-button" onClick={() => {navigate("/events")}}>    
                             View All Past Events
                             <MdArrowForwardIos className="left-arrow" />
                         </div>
                     </div>
+                </div>
+
+                <div className="marquee-div">
+                    <Marquee
+                        limit={20}
+                        text="રામ નામ મેં લીન હૈ દેખત સબમે રામ&nbsp;&nbsp;&nbsp;&nbsp;તાકે પદ વંદન કરે જય જય જલારામ&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;"
+                        color={"yellow"}
+                    />
                 </div>
 
                 <a href="https://www.shreejalarammandir.org/">https://www.shreejalarammandir.org/</a>
