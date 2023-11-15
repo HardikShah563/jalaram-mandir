1c47b66f0ef92b3cca96b9463919ee182a4d0dec
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2023-11-15 12:08:10 +0530

    creating server

---
+++ client/src/config/router.js
@@ -0,0 +1,89 @@
+import { Outlet, createBrowserRouter } from "react-router-dom";
+// importing components
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import SideBar from "../components/SideBar";
+// importing pages
+import Home from "../pages/Home";
+import JalaramBappa from "../pages/JalaramBappa";
+import History from "../pages/History";
+import Team from "../pages/Team";
+import Events from "../pages/Events";
+import Donate from "../pages/Donate";
+import Login from "../pages/Login";
+import DonationEntry from "../pages/DonationEntry";
+import DonationMaster from "../pages/DonationMaster";
+import EventMaster from "../pages/EventMaster";
+import BillTemplate from "../pages/BillTemplate";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <><Navbar /><Outlet /><Footer /></>,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            }, {
+                path: "/about",
+                children: [
+                    {
+                        path: "/about/jalaram-bappa",
+                        element: <JalaramBappa />,
+                    },
+                    {
+                        path: "/about/history",
+                        element: <History />,
+                    }, {
+                        path: "/about/team",
+                        element: <Team />
+                    }
+                ],
+            }, {
+                path: "/events",
+                element: <Events />,
+            }, {
+                path: "/donation",
+                element: <Donate />,
+            }, {
+                path: "/login",
+                element: <Login />,
+            }, {
+                path: "/dashboard",
+                element: <SideBar />,
+                children: [
+                    {
+                        path: "/dashboard",
+                        element: "Hello World"
+                    }, {
+                        path: "/dashboard/donation-entry",
+                        element: <DonationEntry />,
+                    }, {
+                        path: "/dashboard/donation-master",
+                        element: <DonationMaster />,
+                    }, {
+                        path: "/dashboard/event-entry",
+                        element: "event entry",
+                    }, {
+                        path: "/dashboard/event-master",
+                        element: <EventMaster />,
+                    }, {
+                        path: "/dashboard/reciepts",
+                        element: "bill template",
+                    }, {
+                        path: "/dashboard/bill",
+                        element: <BillTemplate />,
+                    }, {
+                        path: "/dashboard/update-bill",
+                        element: "update bill",
+                    }, {
+                        path: "/dashboard/*",
+                        // element: "",
+                    }
+                ],
+            }
+        ],
+    }
+]);
+
+export default router;
