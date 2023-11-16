851c04410f75a1bebe95d3e18793fe1b7cd8d3c8
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2023-11-17 00:27:36 +0530

    basic events page

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,7 +1,31 @@
+// importing from react
+import { useEffect } from "react";
+// importing components
+import UpcomingEvent from "../components/UpcomingEvent";
+import PastEvent from "../components/PastEvent";
+
 export default function Events() {
     return (
         <>
-            <h1>Events</h1>
+            <div className="w-80 mg-a-a">
+                <h1 className="subtitle txt-ctr">Upcoming Events</h1>
+
+                <UpcomingEvent />
+                <UpcomingEvent />
+
+                <h1 className="subtitle txt-ctr">Past Events</h1>
+
+                <PastEvent
+                    eventName={"Jalaram Jayanti"}
+                    eventLocation={"Bhandup"}
+                    eventDate={"10 May 2023"}
+                />
+                <PastEvent
+                    eventName={"Shri Jalaram Jayanti"}
+                    eventLocation={"Mulund"}
+                    eventDate={"14 Nov 2018"}
+                />
+            </div>
         </>
     );
 };
